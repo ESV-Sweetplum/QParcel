0b331b1c79c346b2ce5469c850530fa5d5d89af9
plummyyummy <sweetplumquaver@gmail.com>
2026-04-09 19:43:21 -0700

    properly instantiate filter

--- table/filter.lua
+++ table/filter.lua
@@ -1,7 +1,16 @@
----Filters a table via a given function. For each element in the table, it is passed into the function; if the function returns true, the value is kept, and if the function returns false, the value will not be kept in the new array. Not mutative.
+require("table.duplicate")
+
+---Mutatively filters a table via a given function. For each element in the table, it is passed into the function; if the function returns true, the value is kept, and if the function returns false, the value will not be kept in the new array. Additionally returns the filtered table.
 ---@generic T
 ---@param tbl T[]
 ---@param fn fun(element: T): boolean
 function table.filter(tbl, fn)
     local newTbl = {}
+    for _, v in ipairs(tbl) do
+        if (fn(v)) then table.insert(newTbl, v) end
+    end
+
+    tbl = table.duplicate(newTbl)
+
+    return newTbl
 end
