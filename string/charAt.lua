460cfa3f5cb4e2b768719543cf16734c7ff918a3
plummyyummy <sweetplumquaver@gmail.com>
2026-04-11 17:15:48 -0700

    update charat data

--- string/charAt.lua
+++ string/charAt.lua
@@ -1,7 +1,7 @@
----Returns the `idx`th character in a string. Simply used for shorthand. Also supports negative indexes.
+---Returns the `i`-th character in a string. Simply used for shorthand. Also supports negative indexes.
 ---@param str string The string to search.
----@param idx integer If positive, returns the `i`th character. If negative, returns the `i`th character from the end of the string (e.g. -1 returns the last character).
+---@param i integer If positive, returns the `i`th character. If negative, returns the `i`th character from the end of the string (e.g. -1 returns the last character).
 ---@return string
-function string.charAt(str, idx)
-    return str:sub(idx, idx)
+function string.charAt(str, i)
+    return str:sub(i, i)
 end
