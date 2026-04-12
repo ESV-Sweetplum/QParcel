809d55b945d05c1961451c1c40c07956a15f07b7
plummyyummy <sweetplumquaver@gmail.com>
2026-04-11 17:25:10 -0700

    user infer call

---
+++ table/infer.lua
@@ -0,0 +1,20 @@
+---Converts a string to a primitive given the type to infer from.
+---@generic T: "nil" | "number" | "string" | "boolean" | "table" | "function" | "thread" | "userdata"
+---@param v string? The input value.
+---@param t T The type to infer from.
+---@return T?
+function table.infer(v, t)
+    if (v == nil or t == 'table' or t == 'userdata') then
+        return nil
+    end
+    if (t == 'number') then
+        return tn(v)
+    end
+    if (t == 'boolean') then
+        return truthy(v)
+    end
+    if (t == 'string') then
+        return v
+    end
+    return v
+end
