93996a45da9074bc66659a6c0d34bc22ae08012e
plummyyummy <sweetplumquaver@gmail.com>
2026-05-04 16:23:24 -0700

    change kbm code

---
+++ kbm/keyCombo.lua
@@ -0,0 +1,45 @@
+require('packages.table.contains')
+
+---Returns true if the given key combo returns true depending on the given function (e.g. "Ctrl+Shift+L")
+---@param keyCombo string
+---@param fn `utils.IsKeyPressed` | `utils.IsKeyDown`
+---@return boolean
+function kbm.keyComboCheck(keyCombo, fn)
+    if (imgui.IsAnyItemActive() or not keyCombo or keyCombo == 'NONE') then return false end
+    keyCombo = keyCombo:upper()
+    local comboList = {}
+    for v in keyCombo:gmatch('[%u%d]+') do
+        table.insert(comboList, v)
+    end
+    local keyReq = comboList[#comboList]
+    local ctrlHeld = utils.IsKeyDown(keys.LeftControl) or utils.IsKeyDown(keys.RightControl)
+    local shiftHeld = utils.IsKeyDown(keys.LeftShift) or utils.IsKeyDown(keys.RightShift)
+    local altHeld = utils.IsKeyDown(keys.LeftAlt) or utils.IsKeyDown(keys.RightAlt)
+    if (table.contains(comboList, 'CTRL') ~= ctrlHeld) then
+        return false
+    end
+    if (table.contains(comboList, 'SHIFT') ~= shiftHeld) then
+        return false
+    end
+    if (table.contains(comboList, 'ALT') ~= altHeld) then
+        return false
+    end
+    local keyReqNum = keys[keyReq]
+    if keyReq:find('^%d$') then
+        keyReqNum = tn(keyReq) + 48
+    end
+    if not keyReqNum then
+        return false
+    end
+    return fn(keyReqNum)
+end
+
+function kbm.pressedKeyCombo(keyCombo)
+    kbm.keyComboCheck(keyCombo, utils.IsKeyPressed)
+end
+
+function kbm.holdingKeyCombo(keyCombo)
+    kbm.keyComboCheck(keyCombo, utils.IsKeyDown)
+end
+
+kbm.executedKeyCombo = kbm.pressedKeyCombo
