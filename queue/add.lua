369342a9b690ba57fc189f903fbafedd2e84fdf1
plummyyummy <sweetplumquaver@gmail.com>
2026-05-23 01:52:33 -0700

    ballsd

--- queue/add.lua
+++ queue/add.lua
@@ -1,18 +1,20 @@
 ---@param e QueueElement
 function queue.add(e)
     local function insertIfExists(tbl, item)
-        if (item) then table.insert(tbl, item) end
+        if (not item) then return end
+        table.insert(tbl, item)
     end
 
     local function insertManyIfExists(tbl, items)
-        if (items and truthy(#items)) then
-            for i = 1, #items do
-                local item = items[i]
-                table.insert(tbl, item)
-            end
+        if (not items or not truthy(#items)) then return end
+        for i = 1, #items do
+            local item = items[i]
+            table.insert(tbl, item)
         end
     end
 
     insertIfExists(queue.cur.bmsToAdd, e.AddBookmark)
     insertManyIfExists(queue.cur.bmsToAdd, e.AddBookmarkBatch)
+    insertIfExists(queue.cur.bmsToRemove, e.RemoveBookmark)
+    insertManyIfExists(queue.cur.bmsToRemove, e.RemoveBookmarkBatch)
 end
