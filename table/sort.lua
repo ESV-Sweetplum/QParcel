067eda031c12346e9dc081d215cd2829f5dcc3ec
plummyyummy <sweetplumquaver@gmail.com>
2026-06-04 23:25:56 -0700

    force function not inline to avoid compile error

--- table/sort.lua
+++ table/sort.lua
@@ -3,19 +3,25 @@ require('packages.table.duplicate')
 ---@param a number
 ---@param b number
 ---@return boolean
-function sortAscending(a, b) return a < b end
+function sortAscending(a, b)
+    return a < b
+end
 
 ---Sorting function for sorting objects by their `startTime` property. Should be passed into [`table.sort`](lua://table.sort).
 ---@param a { StartTime: number }
 ---@param b { StartTime: number }
 ---@return boolean
-function sortAscendingStartTime(a, b) return a.StartTime < b.StartTime end
+function sortAscendingStartTime(a, b)
+    return a.StartTime < b.StartTime
+end
 
 ---Sorting function for sorting objects by their `time` property. Should be passed into [`table.sort`](lua://table.sort).
 ---@param a { time: number }
 ---@param b { time: number }
 ---@return boolean
-function sortAscendingTime(a, b) return a.time < b.time end
+function sortAscendingTime(a, b)
+    return a.time < b.time
+end
 
 ---Sorting function for sorting notes by their `startTime` property. Should be passed into [`table.sort`](lua://table.sort). If two items are identical, sorts by their lanes instead.
 ---@param a { StartTime: number, Lane: integer }
