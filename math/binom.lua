54ebd3456488aed11a20229a593926e6f283265f
plummyyummy <sweetplumquaver@gmail.com>
2026-09-18 12:25:06 -0700

    Update binom.lua

--- math/binom.lua
+++ math/binom.lua
@@ -1,6 +1,9 @@
-require('packages.math.factorial')
 ---Returns n choose r, or nCr.
 ---@param n integer
 ---@param r integer
 ---@return integer
-function math.binom(n, r) return math.factorial(n) / (math.factorial(r) * math.factorial(n - r)) end
+function math.binom(n, r)
+    local result = math.factorial(n) / (math.factorial(r) * math.factorial(n - r))
+    ---@cast result integer
+    return result
+end
